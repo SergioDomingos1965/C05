2bb9a257365d1536202521637bc532066318ee49
Sebastião Domingos <sebastiaofranciscodomingos1974@gmail.com>
2025-10-01 21:46:34 +0100

    ex01

---
+++ ex01/form.hpp
@@ -0,0 +1,20 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+#include <string>
+#include "Bureaucrat.hpp"
+    class Form
+    {
+        private:
+
+            const std::string name;
+            bool isSigned;
+            const int gradeToSign;
+            const int gradeToSign;
+            const int gradeToExecute;
+        public:
+            
+            
+    };
+
+#endif
