f308e9f4e953158dfb0e4063f91a8d422a9b8565
Sebastião Domingos <sebastiaofranciscodomingos1974@gmail.com>
2025-10-09 11:29:03 +0100

    feito

---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAU_CRAT_HPP
+#define BUREAU_CRAT_HPP
+#include <iostream>
+#include <exception>
+#include <string>
+#include "form.hpp"
+
+class Form;
+    class Bureaucrat
+    {
+        private: 
+            const std::string name;
+            int grade;
+        public:
+            Bureaucrat(std::string name, int grade);
+            Bureaucrat(const Bureaucrat &src);
+            Bureaucrat &operator=(const Bureaucrat &src);
+            ~Bureaucrat();
+            std::string getName()const;
+            int getGrade()const;
+            void incremetGrade();
+            void decrementGrade();
+            void signForm(Form &form);
+
+        class GradeTooHighException: public std::exception
+        {
+            public:
+                virtual const char *what()const throw();
+        };
+
+        class GradeTooLowException: public std::exception
+        {
+            public:
+                virtual const char *what()const throw();
+        };
+    };
+    std::ostream &operator<<(std::ostream &out, const Bureaucrat &a);
+#endif
